80ecec8de0cd42af9b94429755486e79d8d74f56
Trisert <36551466+Trisert@users.noreply.github.com>
2020-03-28 15:04:31 +0100

    Add files via upload

---
+++ installarch.sh
@@ -0,0 +1,53 @@
+!#/bin/bash
+
+cfdisk
+
+mkfs.vfat -F 32 /dev/sda1
+
+mkswap /dev/sda2
+
+mkfs.xfs /dev/sda3
+
+mkdir /mnt/boot
+
+mount /dev/sda1 /mnt/boot
+
+mount /dev/sda3 /mnt
+
+pacman -Sy reflector 
+
+reflector --sort rate --save /etp/pacman.d/mirrorlist
+
+pacstrap /mnt linux linux-firmware base base-devel
+
+arch-chroot /mnt
+
+hwclock --systohc
+
+ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+
+pacman -Sy nano grub efibootmgr dhcpcd git 
+
+systemctl enable dhcpcd
+
+nano /etc/locale.gen
+
+locale-gen
+
+nano /etc/locale.conf
+
+nano /etc/vconsole.conf
+
+nano /etc/hostname
+
+passwd 
+
+useradd -m -G wheel nicola
+
+passwd nicola
+
+grub-install --target=x86_64-efi --efi-directory=/boot
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
